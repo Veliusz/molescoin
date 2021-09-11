3a7a08e7ae4d08f9c7760aa9c4c3d4b3e3b6bd2f
PRASOPSUK KHENDA <6331305016@lamduan.mfu.ac.th>
2021-09-12 05:04:43 +0700

    update

---
+++ pages/success.vue
@@ -0,0 +1,18 @@
+<template>
+  <body>
+    <div>
+      <center>
+        <img src="../static/suc.png" width="400" height="400" alt="AVATAR" />
+      </center>
+    </div>
+    <div center>
+      <center>
+        <h1>Successful</h1>
+        <br /><br />
+        <v-btn elevation="20" large href="#"> <h1>NEXT</h1></v-btn>
+      </center>
+    </div>
+  </body>
+</template>
+
+
